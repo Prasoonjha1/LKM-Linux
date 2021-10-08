d526b111f02a9005be709e6800191137708562a4
Prasoonjha1 <57556301+Prasoonjha1@users.noreply.github.com>
2021-10-08 13:27:07 +0530

    Update kernelDriver.c

--- kernelDriver.c
+++ kernelDriver.c
@@ -29,10 +29,6 @@ static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
-/** @brief Devices are represented as file structure in the kernel. The file_operations structure from
- *  /linux/fs.h lists the callback functions that you wish to associated with your file operations
- *  using a C99 syntax structure. char devices usually implement open, read, write and release calls
- */
 static struct file_operations fops =
 {
    .open = dev_open,
